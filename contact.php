6f1de2d9c2f2c94caeafc2b743dd9a3e1fd7ce4b
MOHD HAJI <157289794+MOHDHAJI12@users.noreply.github.com>
2024-05-11 16:23:53 +0530

    portfolio

---
+++ contact.php
@@ -0,0 +1,41 @@
+<?php
+//Import PHPMailer classes into the global namespace
+//These must be at the top of your script, not inside a function
+use PHPMailer\PHPMailer\PHPMailer;
+use PHPMailer\PHPMailer\SMTP;
+use PHPMailer\PHPMailer\Exception;
+
+//Load Composer's autoloader
+require 'PHPMailer/PHPMailer.php';
+require 'PHPMailer/SMTP.php';
+require 'PHPMailer/Exception.php';
+
+
+//Create an instance; passing `true` enables exceptions
+$mail = new PHPMailer(true);
+
+try {
+    //Server settings
+    $mail->isSMTP();                                            //Send using SMTP
+    $mail->Host       = 'smtp.gmail.com';                     //Set the SMTP server to send through
+    $mail->SMTPAuth   = true;                                   //Enable SMTP authentication
+    $mail->Username   = 'mohdhajihatims@gmail.com';                     //SMTP username
+    $mail->Password   = 'ardylfumysmvolwy';                               //SMTP password
+    $mail->SMTPSecure = PHPMailer::ENCRYPTION_SMTPS;            //Enable implicit TLS encryption
+    $mail->Port       = 465;                                    //TCP port to connect to; use 587 if you have set `SMTPSecure = PHPMailer::ENCRYPTION_STARTTLS`
+
+    //Recipients
+    $mail->setFrom('mohdhajihatims@gmail.com', 'contact form');
+    $mail->addAddress('mohdhajihatims@gmail.com', 'portfolio');     //Add a recipient
+
+
+    //Content
+    $mail->isHTML(true);                                  //Set email format to HTML
+    $mail->Subject = 'contact form';
+    $mail->Body    = "Sender Name - $name <br> Sender Email -$email <br> subject -$subject  <br> message - $message"
+   
+    $mail->send();
+    echo 'Message has been sent';
+} catch (Exception $e) {
+    echo "Message could not be sent. Mailer Error: {$mail->ErrorInfo}";
+}
